3b69ec80471c9807fcb7e5b91b7a42f6cf4ff211
Masato Miyamoto <project-europa@excite.co.jp>
2020-08-09 13:36:03 +0900

    add: #6 jQueryの表示切り替え処理を追加

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,37 +1,57 @@
 @extends('layouts/app')
 
 @section('title','Bar#180 MENU')
-    
+
 @section('content')
 <!--selectMenu-->
-　<div class="btn-group btn-group-toggle" data-toggle="buttons"> 
+　<div class="btn-group btn-group-toggle" data-toggle="buttons">
     <label id="allMenus" class="btn btn-info active">
-      <input type="radio" name="options" id="option1 radio" autocomplete="off" checked>ALL
+        <input type="radio" name="options" id="option1 radio" autocomplete="off" checked>ALL
     </label>
     <label id="drinks" class="btn btn-info">
-      <input type="radio" name="options" id="option2 radio" autocomplete="off">DRINK
+        <input type="radio" name="options" id="option2 radio" autocomplete="off">DRINK
     </label>
     <label id="foods" class="btn btn-info">
         <input type="radio" name="options" id="option3 raido" autocomplete="off">FOOD
-      </label>
-  </div>
-  <!--selectMenu-end-->
-    <div class='abc'>
-        @foreach ($allItems as $item)
-            <div class='menu'>
-                <dt><img src="{{asset('image/'.$item->image_path)}}"></dt>
-                <dd>{{$item->name}}</dd>
-                <dd>￥{{$item->price}}</dd>
-            </div>
-        @endforeach
+    </label>
+</div>
+<!--selectMenu-end-->
+<div class='abc'>
+    @foreach ($allItems as $item)
+    <div class='menu {{$item->menu_type}}'>
+        <dt><img src="{{asset('image/'.$item->image_path)}}"></dt>
+        <dd>{{$item->name}}</dd>
+        <dd>￥{{$item->price}}</dd>
     </div>
-    
+    @endforeach
+</div>
+
 @endsection
 
 @section('script')
-<script>      
-     document.querySelector(".nav2").classList.add('active');
- </script>
+<script>
+    document.querySelector(".nav2").classList.add('active');
+    $('#allMenus').on('click', function() {
+        if($(this).find('input').prop("checked")) {
+            $('.menu').show()
+        } else {
+            $('.menu').hide()
+        }
+    });
+    $('#drinks').on('click', function() {
+        if($(this).find('input').prop("checked")) {
+            $('.drink').show()
+        } else {
+            $('.drink').hide()
+        }
+    });
+    $('#foods').on('click', function() {
+        if($(this).find('input').prop("checked")) {
+            $('.food').show()
+        } else {
+            $('.food').hide()
+        }
+    });
+</script>
 
 @endsection
-
