7a8060b36e99d8914253b6cc81f970db43a0b0ee
下澤俊樹 <toshiki.s@shimozawatoshikinoMacBook-Pro.local>
2020-08-09 19:11:37 +0900

    20200809r2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
 @section('stylesheet')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
     <link rel="stylesheet" href="{{asset('/css/slick.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/custom.css')}}">
     
 @endsection
     
@@ -12,9 +13,11 @@
  {{--SlickSlider--}}
   
  <ul class="slick01 slick-slider">
-    <li class="slick-slide slick-current slick-active" data-slick-index="1" aria-hidden="false" tabindex="0" role="tabpanel" id="slick-slide01" aria-describedby="slick-slide-control01" style="width: 920px;"><img src="{{asset('image/slider01.jpeg')}}" alt="slider01"></li>
-    <li class="slick-slide slick-current slick-active" data-slick-index="2" aria-hidden="false" tabindex="0" role="tabpanel" id="slick-slide02" aria-describedby="slick-slide-control02" style="width: 920px;"><img src="{{asset('image/slider02.jpeg')}}" alt="slider01"></li>
-    <li class="slick-slide slick-current slick-active" data-slick-index="3" aria-hidden="false" tabindex="0" role="tabpanel" id="slick-slide03" aria-describedby="slick-slide-control03" style="width: 920px;"><img src="{{asset('image/slider03.jpeg')}}"></li>
+    <li class="slick-slide slick-current slick-active" data-slick-index="1" aria-hidden="false" tabindex="0" role="tabpanel" id="slick-slide01" aria-describedby="slick-slide-control01" style="width: 920px;"><img src="{{asset('image/room1.jpg')}}" alt="slider01"></li>
+    <li class="slick-slide slick-current slick-active" data-slick-index="2" aria-hidden="false" tabindex="0" role="tabpanel" id="slick-slide02" aria-describedby="slick-slide-control02" style="width: 920px;"><img src="{{asset('image/room2.jpg')}}" alt="slider01"></li>
+    <li class="slick-slide slick-current slick-active" data-slick-index="3" aria-hidden="false" tabindex="0" role="tabpanel" id="slick-slide03" aria-describedby="slick-slide-control03" style="width: 920px;"><img src="{{asset('image/room3.jpg')}}"></li>
+    <li class="slick-slide slick-current slick-active" data-slick-index="4" aria-hidden="false" tabindex="0" role="tabpanel" id="slick-slide04" aria-describedby="slick-slide-control04" style="width: 920px;"><img src="{{asset('image/room4.jpg')}}"></li>
+    <li class="slick-slide slick-current slick-active" data-slick-index="5" aria-hidden="false" tabindex="0" role="tabpanel" id="slick-slide05" aria-describedby="slick-slide-control05" style="width: 920px;"><img src="{{asset('image/room5.jpg')}}"></li>
     <button class="slick-prev slick-arrow" aria-label="Previous" type="button" style="">Previous</button>
     <button class="slick-next slick-arrow" aria-label="Next" type="button" style="">Next</button>
 </ul>
@@ -34,7 +37,15 @@
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript" src="{{asset('/js/slick.min.js')}}"></script>
-    <script type="text/javascript" src="{{asset('/js/custom.js')}}"></script>
+    <script>
+        $('.slick01').slick({
+            autoplay:true,
+            autoplaySpeed:1000,
+            slidesToShow:2,
+            infinit:true,
+            }
+        );
+    </script>
    　<script>
        document.querySelector('.nav1').classList.add('active');
      </script>
