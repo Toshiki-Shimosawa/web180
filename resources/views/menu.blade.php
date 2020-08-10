beaec11ccd35886522b341e0c8bee4a895045e8c
Masato Miyamoto <project-europa@excite.co.jp>
2020-08-10 09:30:07 +0900

    fix: #8

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,69 +1,46 @@
 @extends('layouts/app')
 
 @section('title','Bar#180 MENU')
-    
+
 @section('content')
 <!--selectMenu-->
-　<div class="btn-group btn-group-toggle" data-toggle="buttons"> 
+　<div class="btn-group btn-group-toggle" data-toggle="buttons">
     <label id="allMenus" class="btn btn-info active">
-      <input type="radio" name="options" id="option1 radio" autocomplete="off" >ALL
+        <input type="radio" name="options" id="option1 radio" autocomplete="off">ALL
     </label>
-    <label id="drinks" class="btn btn-info">
-      <input type="radio" name="options" id="option2 radio" autocomplete="off" >DRINK
+    @foreach ($allItems as $item)
+    <label id="{{$item->menu_type . 's'}}" class="labelCheck btn btn-info">
+        <input type="radio" name="options" autocomplete="off">{{$item->menu_type}}
     </label>
-    <label id="foods" class="btn btn-info">
-        <input type="radio" name="options" id="option3 radio" autocomplete="off" checked>FOOD
-      </label>
-  </div>
-  <!--selectMenu-end-->
-    <div class='abc'>
-        @foreach ($allItems as $item)
-            <div class='menu {{$item->menu_type}}'>
-                <dt><img src="{{asset('image/'.$item->image_path)}}"></dt>
-                <dd>{{$item->name}}</dd>
-                <dd>￥{{$item->price}}</dd>
-            </div>
-        @endforeach
+    @endforeach
+</div>
+<!--selectMenu-end-->
+<div class='abc'>
+    @foreach ($allItems as $item)
+    <div class='menu {{$item->menu_type}}'>
+        <dt><img src="{{asset('image/'.$item->image_path)}}"></dt>
+        <dd>{{$item->name}}</dd>
+        <dd>￥{{$item->price}}</dd>
     </div>
-    
+    @endforeach
+</div>
+
 @endsection
 
 @section('script')
 <script>
     document.querySelector(".nav2").classList.add('active');
-    
-    $('#allMenus').on('click', function() {
-        $('input').prop('checked',false);
-        $(this).prop('checked',true);
-        
-        if($(this).find('input').prop("checked")) {
-            $('.menu').show()
-        } else {
-            $('.menu').hide()
-        }
-    });
-    $('#drinks').on('click', function() {
-        $('input').prop('checked', true);
-        $(this).prop('checked',true);
 
-        if($(this).find('input').prop("checked")) {
-            $('.drink').show()
-        } else {
-            $('.drink').hide()
-        }
+    $('#allMenus').on('click', function() {
+        $('.menu').show()
     });
-    $('#foods').on('click', function() {
-        $('input').prop('checked',false);
-        $(this).prop('checked',true);
-        
-        if($(this).find('input').prop("checked")) {
-            $('.food').show()
-        } else {
-            $('.food').hide()
-        }
+
+    $('labelCheck').on('click', function() {
+        const id = $(this).attr('id').slice(0,-1);
+        $(`.menu.${id}`).show()
+        $(`.menu:not(.${id})`).hide()
     });
 </script>
- 
+
 
 @endsection
-
